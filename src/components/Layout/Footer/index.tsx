a38ef668def5b6f85d7034417c9ff2f24e14cf34
Hugo Levet <hugolevet.pro@gmail.com>
2022-06-20 15:42:08 +0200

    ADD newsletter

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import { ReactComponent as HeartIcon } from '../../../assets/img/heart.svg';
+import DiscordIcon from 'components/Icons/DiscordIcon';
+import TwitterIcon from 'components/Icons/TwitterIcon';
+import style from './footer.module.scss';
+
+// TODO: bind newsletter mailchimp form
 
 const Footer = () => {
   return (
-    <footer className='text-center mt-2 mb-3'>
-      <div>
-        <a
-          {...{
-            target: '_blank'
-          }}
-          className='d-flex align-items-center'
-          href='https://elrond.com/'
-        >
-          Made with <HeartIcon className='mx-1' /> by Elrond Network.
-        </a>
+    <footer id={style['main-footer']}>
+      <div className={style.newsletter}>
+        <h2>Get the latest updates,<br />
+          highlights and team activities</h2>
+        <form action="">
+          <input type="email" placeholder="E-mail..." />
+          <button type="submit">Sign In</button>
+        </form>
+        <div className={style['join-us']}>
+          <h2>Join Us</h2>
+          <div className={style.social}>
+            <div className={style.twitter + ' ' + style.icon}>
+              <TwitterIcon />
+            </div>
+            <div className={style.discord + ' ' + style.icon}>
+              <DiscordIcon />
+            </div>
+          </div>
+        </div>
       </div>
+      {/* TODO: footer with nav etc */}
     </footer>
   );
 };
