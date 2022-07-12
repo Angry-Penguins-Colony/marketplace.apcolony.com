6ca8137256c1573eab61868c3616f8e3f7933b55
Theo F <theo.farnole@gmail.com>
2022-07-12 12:54:29 +0200

    FIX get:queue script

--- scripts/getToBuildQueue.ts
+++ scripts/getToBuildQueue.ts
@@ -1,6 +1,9 @@
+import RenderConfig from "@apc/renderer/dist/classes/RenderConfig";
 import { ApiNetworkProvider } from "@elrondnetwork/erdjs-network-providers/out";
 import ReadGateway from "../src/classes/ReadGateway";
 import config from "../src/config";
+import { userRenderConfig } from "../src/config/render.config";
+import { renderConfigPlugins } from "../src/config/render.plugins.config";
 import { functionNames } from "../src/const";
 
 main();
@@ -8,7 +11,7 @@ main();
 
 async function main() {
     const gateway = new ReadGateway(config.gatewayUrl, config.customisationContract, config.readGatewayOptions);
-    const queue = await gateway.getToBuildQueue();
+    const queue = await gateway.getToBuildQueue([]);
     console.log(queue);
 }
 
