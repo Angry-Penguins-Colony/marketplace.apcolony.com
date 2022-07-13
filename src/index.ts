5084e67b1607680bd71821c14fabe9e9ea18bad4
Theo F <theo.farnole@gmail.com>
2022-07-13 15:53:32 +0200

    ADD config in this package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import ImageRenderer from "./classes/ImageRenderer";
+import RenderConfig from "./classes/RenderConfig";
+import { userConfig, userPlugins } from "./config";
 
-export { ImageRenderer };
+export const renderConfig = new RenderConfig(userConfig, userPlugins);
+const imageRenderer = new ImageRenderer(renderConfig);
+
+export default imageRenderer;
