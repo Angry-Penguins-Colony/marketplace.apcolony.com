bd0229553a2da744b1542e16903f6e004208b89b
Hugo Levet <hugolevet.pro@gmail.com>
2022-07-20 15:23:28 +0200

    ADD preparing bind button

--- src/pages/Customize/index.tsx
+++ src/pages/Customize/index.tsx
@@ -7,7 +7,7 @@ import tmpImgBackground from './../../assets/img/penguin_background.png';
 import style from './customize.module.scss';
 import PenguinRender from './Render';
 
-// bind with real function
+// TODO bind with real function
 function getMyPenguinData(id: string | undefined) {
     if (id === undefined) {
         // TODO: found first penguin of user
@@ -20,9 +20,54 @@ function getMyPenguinData(id: string | undefined) {
     };
 }
 
+interface PenguinItems {
+    background?: string;
+    hat?: string;
+    eyes?: string;
+    clothes?: string;
+    beak?: string;
+    skin?: string;
+    weapon?: string;
+}
+
+
 const Customize = () => {
     const { id } = useParams();
     const penguinData = getMyPenguinData(id);
+
+    const [penguinItems, setPenguinItems] = React.useState<PenguinItems>({});
+
+    React.useEffect(() => {
+        setTimeout(() => { // simulate server call TODO: add api call
+            setPenguinItems({
+                background: tmpImgBackground,
+            });
+        }, 900);
+    }, []);
+
+    function resetItems() {
+        console.log('reset items');
+        setPenguinItems({});
+    }
+
+    function cancelAll() {
+        // TODO: cancel all
+        console.log('cancel all');
+    }
+
+    function saveCustomization() {
+        // TODO: save customization
+        console.log('save customization');
+    }
+
+    function addItem(itemType: string, itemSrc: string | undefined) {
+        // add item
+        const newItems = {
+            ...penguinItems,
+            [itemType]: itemSrc
+        };
+        setPenguinItems(newItems);
+    }
 
     return (
         <>
@@ -34,9 +79,7 @@ const Customize = () => {
                         <div className={style.item + ' ' + style.eyes}></div>
                         <div className={style.item + ' ' + style.clothes}></div>
                     </div>
-                    <PenguinRender items={{
-                        background: tmpImgBackground,
-                    }} />
+                    <PenguinRender items={penguinItems} />
                     <div className={style.items}>
                         <div className={style.item + ' ' + style.beak}></div>
                         <div className={style.item + ' ' + style.skin}></div>
