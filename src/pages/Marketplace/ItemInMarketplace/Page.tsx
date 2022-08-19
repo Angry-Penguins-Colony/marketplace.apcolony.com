46b5f358d293f506ec9d9f73245780b7599d9528
Hugo Levet <hugolevet.pro@gmail.com>
2022-08-19 23:52:33 +0200

    ADD desktop version item in marketplace

--- src/pages/Marketplace/ItemInMarketplace/Page.tsx
+++ src/pages/Marketplace/ItemInMarketplace/Page.tsx
@@ -54,7 +54,10 @@ const Page = (
                 </div>
             </section>
             <section className={style.buy}>
-                <Button onClick={() => { setBuyingPopupIsVisible(true); }}>Buy for {data.price} EGLD</Button>
+                <h2>Price</h2>
+                <p className={style.price}>{data.price} EGLD</p>
+                <Button onClick={() => { setBuyingPopupIsVisible(true); }} type='primary' className={style.desktop + ' ' + style.button}>Buy for {data.price} EGLD</Button>
+                <Button onClick={() => { setBuyingPopupIsVisible(true); }} className={style.mobile + ' ' + style.button}>Buy for {data.price} EGLD</Button>
             </section>
             <section className={style['item-and-activity']}>
                 <ItemsAndActivities getActivities={getActivities} items={data.items ? data.items : []} activities={activities} />
@@ -87,7 +90,7 @@ const Page = (
                                     <img src={data.thumbnail} alt={data.name} />
                                     <div className={style.infos}>
                                         <div className={style.line}>
-                                            <div className={style.label}>Penguin Id</div>
+                                            <div className={style.label}>Penguin ID</div>
                                             <div className={style.value}>{data.name}</div>
                                         </div>
                                         <div className={style.line}>
@@ -115,6 +118,30 @@ const Page = (
                     )
                 }
                 <section>
+                    <h2>Checkout</h2>
+                    <div className={style.infos}>
+                        <div className={style.line}>
+                            <div className={style.label}>Penguin ID</div>
+                            <div className={style.value}>{data.name}</div>
+                        </div>
+                        <div className={style.line}>
+                            <div className={style.label}>Owner</div>
+                            <div className={style.value + ' ' + style.owner}>erd1pxljk7hukrjzq3cc0y00s5zs9r49q8pcc7wn6n8g752nuxpwdmfsxlm5y3</div>
+                        </div>
+                    </div>
+                    {
+                        type === 'penguin' &&
+                        <div className={style['items-attached']}>
+                            <h3>Items attached to the penguin</h3>
+                            <div className={style.content}>
+                                {data.items.map((aItem: any) => {
+                                    return (
+                                        <ItemComponent key={aItem.id} item={aItem} />
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    }
                     <h3>Total</h3>
                     <div className={style.price}>{data.price} EGLD</div>
                     <Button className={style.button} onClick={buyElmt}>Buy</Button>
