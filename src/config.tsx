12029da836e38cba0ace4eea07a2d98a89af202b
Theo F <theo.farnole@gmail.com>
2022-09-07 09:26:58 +0200

    ADD devnet/mainnet API switch w/ .env

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,15 @@
+const useDevnet = process.env.REACT_APP_DEVNET == '1';
+
+if (useDevnet) {
+  console.log('Using devnet');
+}
+
 export const contractAddress =
   'erd1qqqqqqqqqqqqqpgquvt728n40ssd8n2qns9jrlqpwq2jc4rj4cysfuj3ad';
 
 export const dAppName = 'Marketplace';
 export const ifpsGateway = 'https://ipfs.io/ipfs/';
-export const marketplaceApi = 'https://apc-marketplace-api.herokuapp.com/';
+
+const devnetApi = 'https://apc-marketplace-api-devnet.herokuapp.com/';
+const mainnetApi = 'https://apc-marketplace-api.herokuapp.com/';
+export const marketplaceApi = useDevnet ? devnetApi : mainnetApi;
