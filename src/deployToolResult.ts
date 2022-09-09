9cfbc949dcb82d4d0b5bcdd11ded3f0a5840720f
Theo F <theo.farnole@gmail.com>
2022-09-09 12:42:38 +0200

    FIX bad identifier on devnetToolResult

--- src/deployToolResult.ts
+++ src/deployToolResult.ts
@@ -17,42 +17,42 @@ export const devnetToolDeploy = {
         {
             "name": "Oceanic Trench",
             "slot": "background",
-            "identifier": "BACKGROUND-b77385-1"
+            "identifier": "BACKGROUND-b77385-01"
         },
         {
             "name": "Green",
             "slot": "background",
-            "identifier": "BACKGROUND-b77385-2"
+            "identifier": "BACKGROUND-b77385-02"
         },
         {
             "name": "Yellow",
             "slot": "beak",
-            "identifier": "BEAK-4d2e9e-1"
+            "identifier": "BEAK-4d2e9e-01"
         },
         {
             "name": "Coat With Brown Fur",
             "slot": "clothes",
-            "identifier": "CLOTHES-332023-1"
+            "identifier": "CLOTHES-332023-01"
         },
         {
             "name": "Purple Headband",
             "slot": "eyes",
-            "identifier": "EYES-62f369-1"
+            "identifier": "EYES-62f369-01"
         },
         {
             "name": "Green Headband",
             "slot": "eyes",
-            "identifier": "EYES-62f369-2"
+            "identifier": "EYES-62f369-02"
         },
         {
             "name": "Black",
             "slot": "skin",
-            "identifier": "SKIN-09f223-1"
+            "identifier": "SKIN-09f223-01"
         },
         {
             "name": "Spear",
             "slot": "weapon",
-            "identifier": "WEAPON-c4a031-1"
+            "identifier": "WEAPON-c4a031-01"
         }
     ]
 }
