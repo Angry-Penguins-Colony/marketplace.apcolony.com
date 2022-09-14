5f12bfba893d0f6dc85a4f83dd231bc563d9f102
Theo F <theo.farnole@gmail.com>
2022-09-14 10:31:51 +0200

    FIX get render queue

--- src/scripts/getToBuildQueue.ts
+++ src/scripts/getToBuildQueue.ts
@@ -1,11 +1,6 @@
-import RenderConfig from "@apc/renderer/dist/classes/RenderConfig";
-import { ApiNetworkProvider } from "@elrondnetwork/erdjs-network-providers/out";
 import Bottleneck from "bottleneck";
 import ReadGateway from "../classes/ReadGateway";
 import config from "../config";
-import { userRenderConfig } from "../src/config/render.config";
-import { renderConfigPlugins } from "../src/config/render.plugins.config";
-import { functionNames } from "../const";
 
 main();
 
