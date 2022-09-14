a16e2bb89420ce2ba482a4ce52584fcc643ae107
Theo F <theo.farnole@gmail.com>
2022-09-14 10:21:51 +0200

    update devnet.tool-result.ts

--- src/devnet.tool-result.ts
+++ src/devnet.tool-result.ts
@@ -1,58 +1,58 @@
 export const devnetToolDeploy = {
     "customizationContractAddress": {
-        "bech32": "erd1qqqqqqqqqqqqqpgq3knpsaue26jv69kz4ac634m5jaaymc7elx4s86cee7",
-        "pubkey": "000000000000000005008da618779956a4cd16c2af71a8d774977a4de3d9f9ab"
+        "bech32": "erd1qqqqqqqqqqqqqpgqdxgkhhs9q7wt3vvzuu0qxcpenz6rsnaulx4sqcf5uz",
+        "pubkey": "0000000000000000050069916bde05079cb8b182e71e03603998b4384fbcf9ab"
     },
-    "penguinsIdentifier": "PENGUINS-8e3c1c",
+    "penguinsIdentifier": "PENGUINS-47c17e",
     "itemsIdentifier": {
-        "background": "BACKGROUND-8a91da",
-        "beak": "BEAK-683bad",
-        "clothes": "CLOTHES-86d10a",
-        "eyes": "EYES-12c6fc",
-        "hat": "HAT-39360d",
-        "skin": "SKIN-a7c744",
-        "weapon": "WEAPON-167824"
+        "background": "BACKGROUND-1affb9",
+        "beak": "BEAK-6f839d",
+        "clothes": "CLOTHES-a9d0e9",
+        "eyes": "EYES-f773ae",
+        "hat": "HAT-65c91c",
+        "skin": "SKIN-07deaa",
+        "weapon": "WEAPON-cba1fe"
     },
     "items": [
         {
             "name": "Oceanic Trench",
             "slot": "background",
-            "identifier": "BACKGROUND-8a91da-01"
+            "identifier": "BACKGROUND-1affb9-01"
         },
         {
             "name": "Green",
             "slot": "background",
-            "identifier": "BACKGROUND-8a91da-02"
+            "identifier": "BACKGROUND-1affb9-02"
         },
         {
             "name": "Yellow",
             "slot": "beak",
-            "identifier": "BEAK-683bad-01"
+            "identifier": "BEAK-6f839d-01"
         },
         {
             "name": "Coat With Brown Fur",
             "slot": "clothes",
-            "identifier": "CLOTHES-86d10a-01"
+            "identifier": "CLOTHES-a9d0e9-01"
         },
         {
             "name": "Purple Headband",
             "slot": "eyes",
-            "identifier": "EYES-12c6fc-01"
+            "identifier": "EYES-f773ae-01"
         },
         {
             "name": "Green Headband",
             "slot": "eyes",
-            "identifier": "EYES-12c6fc-02"
+            "identifier": "EYES-f773ae-02"
         },
         {
             "name": "Black",
             "slot": "skin",
-            "identifier": "SKIN-a7c744-01"
+            "identifier": "SKIN-07deaa-01"
         },
         {
             "name": "Spear",
             "slot": "weapon",
-            "identifier": "WEAPON-167824-01"
+            "identifier": "WEAPON-cba1fe-01"
         }
     ]
 }
