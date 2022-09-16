6693413729f405b5537882beed420a848fb28184
Theo F <theo.farnole@gmail.com>
2022-09-16 11:49:44 +0200

    FIX not enough gas

--- src/classes/WriteGateway.ts
+++ src/classes/WriteGateway.ts
@@ -112,7 +112,7 @@ export default class WriteGateway {
             func: func,
             args: args,
             value: "",
-            gasLimit: cid.length * 30_000_000,
+            gasLimit: (cid.length * 1.3) * 40_000_000,
             gasPrice: this.networkConfig.MinGasPrice,
             chainID: this.networkConfig.ChainID,
         });
