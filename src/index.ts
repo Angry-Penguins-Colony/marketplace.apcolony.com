5680e53178b1e7747e96354694b440694dc761e5
Theo F <theo.farnole@gmail.com>
2022-09-22 15:36:35 +0200

    RETAKE rename /penguin/:nonce to /penguins/:nonce (same for /item)

--- src/index.ts
+++ src/index.ts
@@ -41,8 +41,8 @@ function start(id: number) {
     app.get("/offers/items/:id", (req, res) => getActivity(req, res, "items"));
 
     app.get('/attributes', (req, res) => getAttributes(req, res, proxyNetwork));
-    app.get('/penguin/:nonce', (req, res) => getPenguin(req, res, proxyNetwork));
-    app.get("/item/:id", (req, res) => getItem(req, res, proxyNetwork));
+    app.get('/penguins/:nonce', (req, res) => getPenguin(req, res, proxyNetwork));
+    app.get("/items/:id", (req, res) => getItem(req, res, proxyNetwork));
 
 
     app.listen(port, () => {
