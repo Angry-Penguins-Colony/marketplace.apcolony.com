ca161a77ed50c620b2cba5f0249a306de61e579f
Hugo Levet <hugolevet.pro@gmail.com>
2022-09-26 23:25:51 +0200

    FIX react 1.18 index

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import './index.css';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/sass/theme.scss';
 import 'dotenv/config';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container as HTMLElement);
+root.render(<App />);
