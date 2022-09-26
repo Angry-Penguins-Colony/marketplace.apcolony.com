9b30597e3c26bed3016ee74a8667cd1a83f183df
Theo F <theo.farnole@gmail.com>
2022-09-26 12:27:20 +0200

    ADD SellPayloadBuilder

---
+++ src/sdk/transactionsBuilders/sell/SellPayloadBuilder.test.ts
@@ -0,0 +1,38 @@
+import { Address } from '@elrondnetwork/erdjs/out';
+import BigNumber from 'bignumber.js';
+import { SellPayloadBuilder } from './SellPayloadBuilder';
+
+const tokenId = {
+    ascii: 'HAT-a1a1a1',
+    hex: '4841542d613161316131',
+}
+
+const smartContract = {
+    bech32: 'erd1qqqqqqqqqqqqqpgq705fxpfrjne0tl3ece0rrspykq88mynn4kxs2cg43s',
+    hex: '00000000000000000500f3e893052394f2f5fe39c65e31c024b00e7d9273ad8d'
+}
+
+test('test', () => {
+    const actual = new SellPayloadBuilder()
+        .setToken(tokenId.ascii, 1)
+        .setMarketplaceSc(Address.fromBech32(smartContract.bech32))
+        .setPrice(new BigNumber('1e18'))
+        .build();
+
+    // example: https://explorer.elrond.com/transactions/b2b6a7975a38077e6c0ca121203121f8755db6cf584149e2c9ba65f7ded61b90#smart
+
+    const expected = 'ESDTNFTTransfer'
+        + '@' + tokenId.hex // token id
+        + '@' + '01' // nonce
+        + '@' + '01' // quantity
+        + '@' + smartContract.hex // receiver
+        + '@' + '616374696f6e546f6b656e' // actionToken
+        + '@' + '0de0b6b3a7640000' // (min_bid) 1 EGLD
+        + '@' + '0de0b6b3a7640000' // (max_bid) 1 EGLD
+        + '@' + 'b2d05e00' // (deadline) 3_000_000_000
+        + '@' + '45474c44' // (accepted_payment_token) EGLD
+        + '@' + '' //  (opt_min_bid_diff) None
+        + '@' + '01' // (opt_sft_max_one_per_payment) True
+
+    expect(actual.toString()).toStrictEqual(expected);
+})
---
+++ src/sdk/transactionsBuilders/sell/SellPayloadBuilder.ts
@@ -0,0 +1,50 @@
+import { AddressValue, ArgSerializer, BigIntValue, BooleanValue, BytesValue, IAddress, OptionType, OptionValue, TransactionPayload, U64Type, U64Value } from '@elrondnetwork/erdjs/out';
+import BigNumber from 'bignumber.js';
+
+export class SellPayloadBuilder {
+
+    private tokenCollection = '';
+    private tokenNonce = -1;
+    private marketplaceSc?: IAddress;
+    private price: BigNumber;
+
+    public setToken(collection: string, nonce: number): SellPayloadBuilder {
+        this.tokenCollection = collection;
+        this.tokenNonce = nonce;
+        return this;
+    }
+
+    public setMarketplaceSc(marketplaceSc: IAddress): SellPayloadBuilder {
+        this.marketplaceSc = marketplaceSc;
+        return this;
+    }
+
+    public setPrice(price: BigNumber): SellPayloadBuilder {
+        this.price = price;
+        return this;
+    }
+
+    build(): TransactionPayload {
+
+        if (!this.marketplaceSc) throw new Error('marketplaceSc is required');
+
+        const args = [
+            BytesValue.fromUTF8(this.tokenCollection),
+            new U64Value(this.tokenNonce),
+            new U64Value(1), // quantity
+            new AddressValue(this.marketplaceSc),
+            BytesValue.fromUTF8('actionToken'),
+            new BigIntValue(this.price),
+            new BigIntValue(this.price),
+            new U64Value(3_000_000_000), // deadline
+            BytesValue.fromUTF8('EGLD'), // accepted payment token
+            new OptionValue(new OptionType(new U64Type())), // opt_min_bid_diff
+            new BooleanValue(true), // opt_sft_max_one_per_payment
+        ];
+
+        const { argumentsString } = new ArgSerializer().valuesToString(args);
+        const data = 'ESDTNFTTransfer@' + argumentsString;
+
+        return new TransactionPayload(data);
+    }
+}
