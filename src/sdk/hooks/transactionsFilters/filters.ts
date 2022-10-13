9ba38ce1f675c348cb3da452b35273741e203380
TF <theo.farnole.pro@gmail.com>
2022-10-13 17:32:00 +0200

    RETAKE minors code changements in filters.ts

--- src/sdk/hooks/transactionsFilters/filters.ts
+++ src/sdk/hooks/transactionsFilters/filters.ts
@@ -3,6 +3,6 @@ import ITransactionFilter from './ITransactionFilter';
 export default class SellTransactionFilter implements ITransactionFilter {
     contains(data: string): boolean {
         const endpointName = Buffer.from('auctionToken', 'utf-8').toString('hex');
-        return data.toString().includes(endpointName);
+        return data.includes(endpointName);
     }
 }
