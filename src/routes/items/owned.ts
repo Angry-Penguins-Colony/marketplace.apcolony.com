32a38590685405456a7d2275399c8e921712a5b9
TF <theo.farnole.pro@gmail.com>
2022-10-14 18:00:02 +0200

    REMOVE log in getOwnedItems

--- src/routes/items/owned.ts
+++ src/routes/items/owned.ts
@@ -1,15 +1,11 @@
 import { Address } from '@elrondnetwork/erdjs/out';
 import { Request, Response } from 'express';
 import { APCNetworkProvider } from '../../classes/APCNetworkProvider';
-import { itemsCollection, itemsDatabase } from '../../const';
 import { sendSuccessfulJSON } from '../../utils/response';
 
 export default async function getOwnedItems(req: Request, res: Response, gatewayProvider: APCNetworkProvider) {
 
     const address = new Address(req.params.bech32);
-
-
     const items = await gatewayProvider.getItemsOfAccount(address);
-    console.log("Found", items.length, "items.");
     sendSuccessfulJSON(res, items);
 }
