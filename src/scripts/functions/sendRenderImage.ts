d98e95f98720ccad74a98901fb31f07655e6d7fb
TF <theo.farnole.pro@gmail.com>
2022-10-14 11:18:39 +0200

    ADD argument images amount in send:RenderImage

--- src/scripts/functions/sendRenderImage.ts
+++ src/scripts/functions/sendRenderImage.ts
@@ -9,22 +9,8 @@ import Bottleneck from "bottleneck";
 import { devnetToolDeploy } from "../../devnet.tool-result";
 import { renderConfig } from "@apcolony/renderer";
 
-export async function sendRenderImage(attributes: RenderAttributes) {
-
+export async function sendRenderImage(attributes: RenderAttributes, writeGateway: WriteGateway) {
     const smartContract = new SmartContract({ address: config.customisationContract });
-
-    const sender = {
-        address: process.env.SENDER_TEST_BECH32,
-        pem: process.env.SENDER_TEST_PEM
-    };
-
-    if (!sender.address) throw new Error("Missing env SENDER_TEST_ADDRESS");
-    if (!sender.pem) throw new Error("Missing env SENDER_TEST_PEM");
-
-    console.log(`Sending render image request to ${sender.address}...`);
-    const writeGateway = new WriteGateway(config.gatewayUrl, new Address(sender.address), UserSigner.fromPem(sender.pem), new Bottleneck());
-
-    await writeGateway.sync();
 
     const tx = smartContract.call({
         func: { name: "renderImage" },
@@ -32,11 +18,11 @@ export async function sendRenderImage(attributes: RenderAttributes) {
             new StringValue(attributes.toAttributes(devnetToolDeploy.items, renderConfig.slots))
         ],
         value: 1_000_000_000_000_000, // 0.001 EGLD
-        gasLimit: 50_000_000,
+        gasLimit: 600_000_000,
         gasPrice: writeGateway.networkConfig.MinGasPrice,
         chainID: writeGateway.networkConfig.ChainID,
     });
 
-
     return writeGateway.sendTransaction(tx);
 }
+
--- src/scripts/sendRenderImageRandom.ts
+++ src/scripts/sendRenderImageRandom.ts
@@ -5,21 +5,53 @@
 import RenderAttributes from "@apcolony/renderer/dist/classes/RenderAttributes";
 import { userConfig } from "@apcolony/renderer/dist/config";
 import { getRandomAttributes } from "@apcolony/renderer/dist/utils/random";
+import { UserSigner } from "@elrondnetwork/erdjs-walletcore/out";
+import { Address } from "@elrondnetwork/erdjs/out";
+import Bottleneck from "bottleneck";
+import WriteGateway from "../classes/WriteGateway";
+import config from "../config";
 import { sendRenderImage } from "./functions/sendRenderImage";
 
 main();
 
 async function main() {
 
+    const imagesToSend = process.argv[2] ? parseInt(process.argv[2]) : 1;
+
+    if (isNaN(imagesToSend)) throw new Error("Invalid number of images to send");
+
+    const gateway = await initializeWriteGateway();
+    console.log(`Sending ${imagesToSend} images with ${gateway.senderAddress.bech32()}...`);
+
     const defaultLayersIds = Object.values(userConfig.defaultLayers ?? {});
 
-    const attributes = new RenderAttributes(getRandomAttributes(
-        userConfig.itemsCID,
-        defaultLayersIds
-    ), []);
+    for (let i = 0; i < imagesToSend; i++) {
+        const attributes = new RenderAttributes(getRandomAttributes(
+            userConfig.itemsCID,
+            defaultLayersIds
+        ), []);
 
-    const { hash } = await sendRenderImage(attributes);
+        const { hash } = await sendRenderImage(attributes, gateway);
 
-    console.log(`Transaction hash: ${hash}`);
+        console.log(`Transaction hash: ${hash}`);
+    }
+
+    console.log("Done!");
 }
 
+
+async function initializeWriteGateway() {
+    const sender = {
+        address: process.env.SENDER_TEST_BECH32,
+        pem: process.env.SENDER_TEST_PEM
+    };
+
+    if (!sender.address) throw new Error("Missing env SENDER_TEST_ADDRESS");
+    if (!sender.pem) throw new Error("Missing env SENDER_TEST_PEM");
+
+    const writeGateway = new WriteGateway(config.gatewayUrl, new Address(sender.address), UserSigner.fromPem(sender.pem), new Bottleneck());
+
+    await writeGateway.sync();
+
+    return writeGateway;
+}
