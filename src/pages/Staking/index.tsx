9e3b63eb9ebff3f01c4b27578eb3d7f5721d3063
DjackSounds <40246661+DjackSounds@users.noreply.github.com>
2022-10-19 16:51:35 +0200

    ADD the staking page

---
+++ src/pages/Staking/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import APCLogoWhite from 'assets/img/apc-logo/white.png';
+import Button from 'components/Abstract/Button/Button';
+import style from './index.module.scss';
+
+export default function Staking() {
+  return (
+    <div id={style.launchpadVente}>
+      Staking
+    </div>
+  )
+}
