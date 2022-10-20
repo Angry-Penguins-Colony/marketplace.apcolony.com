abcca7e2188c34bc1f2c3dbb78d505e835578f18
TF <theo.farnole.pro@gmail.com>
2022-10-20 09:56:24 +0200

    use link in BigCategory component

--- src/components/Navigation/BigCategory/BigCategory.tsx
+++ src/components/Navigation/BigCategory/BigCategory.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import Button from 'components/Abstract/Button/Button';
 import style from './BigCategory.module.scss';
 
@@ -15,7 +16,9 @@ export const BigCategory = ({
         <div className={style['big-category'] + ' w-100'} onClick={() => { window.location.href = link; }}>
             <img src={backgroundImg} alt={title} />
             <h2>{title}</h2>
-            <Button type='normal' onClick={() => { window.location.href = link; }} className={style.button}>View all</Button>
+            <Link to={link}>
+                <Button type='normal' link={link} className={style.button}>View all</Button>
+            </Link>
         </div>
     );
 };
