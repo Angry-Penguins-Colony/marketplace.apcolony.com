c26d08b7bef18c48babae23e32dc0e43999a9d68
DjackSounds <40246661+DjackSounds@users.noreply.github.com>
2022-10-20 11:19:54 +0200

    ADD top staking page mobile

--- src/pages/Staking/index.tsx
+++ src/pages/Staking/index.tsx
@@ -6,7 +6,19 @@ import style from './index.module.scss';
 export default function Staking() {
   return (
     <div id={style.launchpadVente}>
-      Staking
+      <section className={style['top-page']}>
+        <img src={APCLogoWhite} alt="" className="logo" />
+        <h1>Welcome to one of the most <br /> important places of the <br /> Angry Penguins colony…</h1>
+        <div className={style['cta']}>
+          <p>Here is the place where you can Stake your Angry Penguins and receive the colony’s token!</p>
+          <Button className={style.button} type='primary-outline'>STAKE</Button>
+        </div>
+      </section>
+      
+      <section className={style['rewards']}>
+
+      </section>
     </div>
+
   )
 }
