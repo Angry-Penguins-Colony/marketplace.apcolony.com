4e83b73fe268383625b521387d518159107529c7
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2022-10-27 16:29:55 +0200

    FIX market data handle low values

--- src/components/Inventory/MarketData/index.tsx
+++ src/components/Inventory/MarketData/index.tsx
@@ -15,15 +15,15 @@ const MarketData = ({
     return (
         <div className={style['market-data']}>
             <div className={style.item}>
-                <div className={style.value}>{floorPrice.toString()} EGLD</div>
+                <div className={style.value}>{numberToString(floorPrice)} EGLD</div>
                 <div className={style.label}>Floor Price</div>
             </div>
             <div className={style.item}>
-                <div className={style.value}>{totalVolume} EGLD</div>
+                <div className={style.value}>{numberToString(totalVolume)} EGLD</div>
                 <div className={style.label}>Total Volume</div>
             </div>
             <div className={style.item}>
-                <div className={style.value}>{averagePrice} EGLD</div>
+                <div className={style.value}>{numberToString(averagePrice)} EGLD</div>
                 <div className={style.label}>Average Price</div>
             </div>
             <div className={style.item}>
@@ -32,6 +32,10 @@ const MarketData = ({
             </div>
         </div>
     );
+
+    function numberToString(n: number) {
+        return parseFloat(n.toFixed(3)).toString();
+    }
 };
 
 export default MarketData;
