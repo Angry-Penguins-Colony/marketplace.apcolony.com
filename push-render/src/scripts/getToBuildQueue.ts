8be83866bd3892e7eed2e88afa4020c54695fb68
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2022-10-31 10:11:35 +0100

    FIX get:queue

--- push-render/src/scripts/getToBuildQueue.ts
+++ push-render/src/scripts/getToBuildQueue.ts
@@ -3,15 +3,22 @@ import ReadGateway from "../classes/ReadGateway";
 import config from "../config";
 import { devnetToolDeploy } from "../devnet.tool-result";
 import { renderConfig } from "@apcolony/renderer";
+import RenderAttributes from "@apcolony/renderer/dist/classes/RenderAttributes";
 
 main();
 
 
 async function main() {
     const gateway = new ReadGateway(config.gatewayUrl, config.customisationContract, new Bottleneck());
-    const queue = await gateway.getToBuildQueue([]);
+    const queue: {
+        renderAttribute: RenderAttributes;
+        badgeNumber: number;
+    }[] = await gateway.getToBuildQueue();
 
-    console.log(queue.map(a => a.toAttributes(devnetToolDeploy.items, renderConfig.slots)));
+
+    for (const { renderAttribute, badgeNumber } of queue) {
+        console.log(badgeNumber, "=>", renderAttribute.toAttributes(devnetToolDeploy.items, renderConfig.slots));
+    }
     console.log("Found " + queue.length + " images in the queue");
 }
 
