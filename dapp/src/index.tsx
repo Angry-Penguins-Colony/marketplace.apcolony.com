985da71a38380d01357f49b4b3dcb3b457fefb76
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2022-10-31 19:05:26 +0100

    REMOVE dotenv/config in dapp

--- dapp/src/index.tsx
+++ dapp/src/index.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/sass/theme.scss';
-import 'dotenv/config';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container as HTMLElement);
