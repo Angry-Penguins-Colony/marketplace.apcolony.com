bb762a1ca39b40c476656996653b6a69a21c1f1d
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2022-11-03 14:52:02 +0100

    FIX error in StakePopup

--- dapp/src/components/Foreground/Popup/StakePopup/StakePopup.tsx
+++ dapp/src/components/Foreground/Popup/StakePopup/StakePopup.tsx
@@ -7,7 +7,6 @@ import { LOADER } from 'components/Images/ReactImageAppear/constants';
 import ItemsInventory from 'components/Inventory/ItemsInventory/ItemsInventory';
 import NavigationStakedType from 'components/Inventory/NavigationType/NavigationStakingType';
 import { penguinCollection, stakingContract } from 'config';
-import useGetUserOwnedAmount from 'sdk/hooks/api/useGetUserOwnedAmount';
 import useGetStakingInventory from 'sdk/hooks/pages/useGetStakingInventory';
 import stakeTransactionBuilder from 'sdk/transactionsBuilders/staking/stakeTransactionBuilder';
 import Popup from '../Generic/Popup';
@@ -21,16 +20,15 @@ const StakePopup = (
         setTransactionSessionId,
         penguinsStakedArray,
         penguinsUnstakedArray
-    }:{
+    }: {
         isVisible: boolean,
         closeModal: () => void,
         setTransactionSessionId: (sessionId: string | null) => void,
-        penguinsStakedArray : Array<IPenguin> | undefined,
-        penguinsUnstakedArray : Array<IPenguin> | undefined
+        penguinsStakedArray: Array<IPenguin> | undefined,
+        penguinsUnstakedArray: Array<IPenguin> | undefined
     }
 ) => {
     const { address: connectedAddress } = useGetAccountInfo();
-    const ownedAmount = useGetUserOwnedAmount();
 
     const {
         inventoryType,
@@ -39,17 +37,17 @@ const StakePopup = (
         stakedPenguinsCount,
         setInventoryType
     } = useGetStakingInventory(connectedAddress, penguinsStakedArray, penguinsUnstakedArray);
-    
 
-    const stake= new stakeTransactionBuilder();
+
+    const stake = new stakeTransactionBuilder();
     stake.setStakingContract(stakingContract);
 
-    const stakeFunc = async (type:string, itemNonce : number) => {    
-          
-        stake.setStaking({collection: penguinCollection, nonce: itemNonce, connectedAddress : connectedAddress});
+    const stakeFunc = async (type: string, itemNonce: number) => {
+
+        stake.setStaking({ collection: penguinCollection, nonce: itemNonce, connectedAddress: connectedAddress });
         const transaction = stake.build(type);
-        
-        await refreshAccount();        
+
+        await refreshAccount();
 
         const { sessionId } = await sendTransactions({
             transactions: transaction,
@@ -61,29 +59,28 @@ const StakePopup = (
             redirectAfterSign: false
         });
 
-        if (sessionId != null) {           
+        if (sessionId != null) {
             setTransactionSessionId(sessionId);
         }
     }
 
     return <Popup haveCloseButton={true} isVisible={isVisible} onCloseClicked={closeModal}  >
-                {penguinsStakedArray && penguinsUnstakedArray ?  
-                <>
-                    <NavigationStakedType className={style['navigation-type']} onChangeType={setInventoryType} itemsType={inventoryType} stakedPenguinsCount={stakedPenguinsCount} unstakedPenguinsCount={penguinsCount} />
-                    <ItemsInventory
-                            className={style['items-inventory']}
-                            items={inventoryElements}
-                            type='penguins'
-                            amountById={ownedAmount ? ownedAmount['penguins'] : {}}
-                            hasFilter={false}
-                            displayStakingStatus={true}
-                            isStaked={inventoryType === 'staked' ? true : false}
-                            stakingFunction={stakeFunc}
-                    /> 
-                </>     
-                : 
-                <img src={LOADER} alt="loader" className={style['loader']}  />}
-     </Popup>
+        {penguinsStakedArray && penguinsUnstakedArray ?
+            <>
+                <NavigationStakedType className={style['navigation-type']} onChangeType={setInventoryType} itemsType={inventoryType} stakedPenguinsCount={stakedPenguinsCount} unstakedPenguinsCount={penguinsCount} />
+                <ItemsInventory
+                    className={style['items-inventory']}
+                    items={inventoryElements}
+                    type='penguins'
+                    hasFilter={false}
+                    displayStakingStatus={true}
+                    isStaked={inventoryType === 'staked' ? true : false}
+                    stakingFunction={stakeFunc}
+                />
+            </>
+            :
+            <img src={LOADER} alt="loader" className={style['loader']} />}
+    </Popup>
 }
 
 export default StakePopup;
