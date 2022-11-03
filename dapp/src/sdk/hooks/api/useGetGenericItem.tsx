5b6bc1e6b2af8940d70d81ceedef72af715d3b52
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2022-11-03 14:51:08 +0100

    REMOVE ?owned params in /penguins/penguin/:id in dApp hook api call

--- dapp/src/sdk/hooks/api/useGetGenericItem.tsx
+++ dapp/src/sdk/hooks/api/useGetGenericItem.tsx
@@ -1,14 +1,11 @@
 import { IGenericElement } from '@apcolony/marketplace-api';
-import { useGetAccountInfo } from '@elrondnetwork/dapp-core/hooks';
 import useGenericAPICall, { IGenericAPIOptions } from 'sdk/hooks/api/useGenericAPICall';
 import CategoriesType from 'sdk/types/CategoriesType';
 
 export function useGetGenericItem(type: CategoriesType, id: string, options?: IGenericAPIOptions) {
 
-    const { address } = useGetAccountInfo();
-
     const singularType = type == 'penguins' ? 'penguin' : 'item';
-    const raw = useGenericAPICall<IGenericElement>(`${type}/${singularType}/${id}` + (address != '' ? `?owner=${address}` : ''), options);
+    const raw = useGenericAPICall<IGenericElement>(`${type}/${singularType}/${id}`, options);
 
     return raw;
 }
