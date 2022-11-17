d0bd434d5f2a8a4bcc17fff7a520bfe5d6fbdf12
DjackSounds <40246661+DjackSounds@users.noreply.github.com>
2022-11-17 14:48:07 +0100

    MODIFY staking address and abi

--- packages/database/src/devnet.ts
+++ packages/database/src/devnet.ts
@@ -19,7 +19,7 @@ const devnetConfig: Config = {
     eggsCollection: eggsCollection,
     customisationContractAddress: DeployDevnet.customizationContractAddress.bech32,
     sellingContract: "erd1qqqqqqqqqqqqqpgqxygtu607umpgtnp3xwkskeqsyhc26ej0v4xqyglrkj",
-    stakingContract: "erd1qqqqqqqqqqqqqpgqdcjdvpvncw7s8ug56rehyvl8tehk3vl368mqxa7llg",
+    stakingContract: "erd1qqqqqqqqqqqqqpgq4r5lhttf3s98na2vxmk7qyk2t8qs79wt68mq0qs6z3",
     nftStakingToken: "TEST-17e1db",
     originalTokensAmountInStakingSc: 1000000, //Todo : Find a better way to calculate tokens generated
 };
