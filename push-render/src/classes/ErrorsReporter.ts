f108e5847c9804e8985e5f1ea427c2fc9562c08f
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2022-11-21 18:00:12 +0100

    FIX reportError is not defined

--- push-render/src/classes/ErrorsReporter.ts
+++ push-render/src/classes/ErrorsReporter.ts
@@ -43,7 +43,7 @@ export function enableReporting() {
     }
 
     process.on('uncaughtException', function (exception) {
-        reportError(exception);
+        reportErrorToMail(exception);
     });
 }
 
