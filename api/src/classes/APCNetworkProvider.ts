2a0058115c9c74c1ba3606a495de3da2866e235b
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2022-11-24 10:55:45 +0100

    REMOVE log in api endpoint

--- api/src/classes/APCNetworkProvider.ts
+++ api/src/classes/APCNetworkProvider.ts
@@ -146,7 +146,6 @@ export class APCNetworkProvider {
                     break;
 
                 case eggsCollection: {
-                    console.log(nft.nonce);
                     const egg: IOwnedEgg = {
                         ownedAmount: nft.supply.toNumber(),
                         ...this.getEggsFromNft(nft)
