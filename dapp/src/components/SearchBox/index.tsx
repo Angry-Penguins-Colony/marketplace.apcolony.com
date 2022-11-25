dcf1e05e74dfee0da6f57866318cd8e97f9fa682
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2022-11-25 14:55:25 +0100

    hide search icon

--- dapp/src/components/SearchBox/index.tsx
+++ dapp/src/components/SearchBox/index.tsx
@@ -5,20 +5,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 interface Props {
     className?: string;
     onSearch: (search: string) => void;
+    showButton?: boolean;
 }
 
 export const SearchBox = ({
     className = '',
     onSearch,
+    showButton = false
 }: Props) => {
     return (
         <div className='d-flex'>
-            <input type="text" className={'form-control' + ' ' + className} name="x" id="search" placeholder="Search" onChange={(e) => onSearch(e.target.value)} />
-            <span className="input-group-btn">
-                <button className="btn btn-default" type="button">
-                    <FontAwesomeIcon icon={faSearch} />
-                </button>
-            </span>
+            <input type="text" className={'form-control' + ' ' + className} name="x" id="search" placeholder="Type to search" onChange={(e) => onSearch(e.target.value)} />
+
+            {showButton &&
+                <span className="input-group-btn">
+                    <button className="btn btn-default" type="button">
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </span>
+            }
         </div>
     );
 };
