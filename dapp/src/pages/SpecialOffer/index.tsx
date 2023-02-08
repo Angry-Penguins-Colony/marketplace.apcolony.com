f65dbc8759530308462af3affa1bde7d41875fbf
Theo F <theo.farnole@gmail.com>
2023-02-08 11:36:51 +0100

    ADD ItemPageLayout to SpecialOfferPage

--- dapp/src/pages/SpecialOffer/index.tsx
+++ dapp/src/pages/SpecialOffer/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import ItemPageLayout from 'components/Layout/ItemPageLayout';
 
 const SpecialOfferPage = () => {
 
-    return 'Not implemented yet';
+    return <ItemPageLayout
+        itemData={{
+            url: 'https://apc-items.s3.eu-west-3.amazonaws.com/thumbnail_web/15-thumbnail-web.jpg',
+            displayName: 'Chewing Gum'
+        }}>
+
+    </ItemPageLayout >
 }
 
 export default SpecialOfferPage;
