35bbe758bc775e06f55bb72113b189de89dcf3ae
Theo F <theo.farnole@gmail.com>
2023-02-10 15:54:39 +0100

    FIX price display in new sale

--- api/src/classes/ABIParser.ts
+++ api/src/classes/ABIParser.ts
@@ -80,7 +80,6 @@ export async function parseNewSaleData(response: any, getToken: (identifier: str
         },
 
         price: new BigNumber(priceNoDecimals)
-            .div(10 ** inputToken.decimals)
             .toString(),
         token: inputToken
     };
