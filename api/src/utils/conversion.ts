8ad5dbad5167784f94a2203518f0b40f2f9fa91c
Sitaru Alexandru <69410259+sitaru-a@users.noreply.github.com>
2023-08-31 13:18:27 +0300

    Update conversion.ts

--- api/src/utils/conversion.ts
+++ api/src/utils/conversion.ts
@@ -1,4 +1,4 @@
-import { Nonce } from '@elrondnetwork/erdjs-network-providers/out/primitives';
+import { Nonce } from '@multiversx/sdk-network-providers/out/primitives';
 
 export function toIdentifier(collection: string, nonce: number): string {
     return `${collection}-${new Nonce(nonce).hex()}`;
